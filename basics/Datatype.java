58f6da80e05878e7ebc5112086ac0f865f53b097
jenishaprincy <jenishaprincy2112@gamil.com>
2025-08-18 17:21:08 +0530

    Datatypes

---
+++ basics/Datatype.java
@@ -0,0 +1,19 @@
+public class Datatype {
+    public static void main(String[] args) {
+        byte age=18;
+		short page=567;
+		int salary=700;
+		long phone=7010501291l;
+		float weight=45.500f;
+		double location=456.989043;
+		boolean value=true;
+		System.out.println(age);
+		System.out.println(page);
+		System.out.println(salary);
+		System.out.println(phone);
+		System.out.printf("%.2f \n",weight);
+		System.out.printf("%.4f \n",location);
+		System.out.println(value);
+    }
+
+}
